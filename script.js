249b8914001a6334f47db99d90cd407fbb6b8558
rahangdaledipanshu9-hub <rahangdaledipanshu9@gmail.com>
2026-03-14 11:04:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,19 @@ infoBox.innerText = stage.dataset.info;
 
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior: "smooth"
+});
+
+});
+
+});
 /* SCROLL ANIMATION */
 
 const sections = document.querySelectorAll(".section");
